384addb3ef9f2cafa8a572f30cafcc58ddc49315
Manel Ibáñez <mibanez@biosystems.es>
2017-06-02 12:39:48 +0000

    Sum.cs edited online with Bitbucket

--- Summer/Sum.cs
+++ Summer/Sum.cs
@@ -21,8 +21,8 @@
      * 2.- Dado 1 solo valor, obtendremos el mismo valor.
      * 3.- Dados 2 valores, separados por coma (','), los suma. Ejemplo: 1,2 --> Resultado: 3
      * 4.- Ampliar para sumar N valores.
-     * 5.- El separador puede ser tanto una coma (',') como un Carre Return ('\n'). Ejemplo: 1,2\n3 --> Resultado: 6
-     * 6.- Podemos añadirle un separador nuevo en la misma linea, usando el formado: //nuevo_separador\nValor1,Valor2... Ejemplo: //.\n1,2\n3.4 --> Resultado: 10.
+     * 5.- El separador puede ser tanto una coma (',') como un Carrier Return ('\n'). Ejemplo: 1,2\n3 --> Resultado: 6
+     * 6.- Podemos añadirle un separador nuevo en la misma linea, usando el formato: //nuevo_separador\nValor1,Valor2... Ejemplo: //.\n1,2\n3.4 --> Resultado: 10.
      */
     public class Sum
     {
